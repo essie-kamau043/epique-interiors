046b6e9935002444011fd57ee727c898eb87fcd4
essie-kamau043 <156332730+essie-kamau043@users.noreply.github.com>
2026-03-05 19:12:46 +0300

    contact file changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -86,7 +86,7 @@ export default function Contact() {
               </div>
             </div>
           ))}
-          <div className="contact-map">📍 Google Map Embed Here</div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40140.67793334754!2d37.08205438068172!3d-1.05988413179918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f4fb53d8f5581%3A0x4261933def037bb6!2sMakongeni%2C%20Thika!5e1!3m2!1sen!2ske!4v1772726987567!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
       </div>
